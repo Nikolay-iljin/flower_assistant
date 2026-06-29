ce8c588167b5cf4b40ca604877ea70398a3e3b1a
Nikolay-iljin <showtomid@gmail.com>
2026-06-29 22:51:12 +0700

    Create base.html

--- flowers/urls.py
+++ flowers/urls.py
@@ -4,4 +4,7 @@ from flowers import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('addpage/', views.add_page, name='add_page'),
+    path('contacts/', views.contacts, name='contact'),
 ]

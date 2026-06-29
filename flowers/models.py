6bf05ed418caf659c1d385e743860db16e3d81f6
Nikolay-iljin <showtomid@gmail.com>
2026-06-29 23:18:02 +0700

    add model Category

--- flowers/models.py
+++ flowers/models.py
@@ -16,3 +16,14 @@ class Flowers(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})

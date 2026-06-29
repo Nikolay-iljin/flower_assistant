f02367b87e50b0de7571ccf897ec62f4b58932a4
Nikolay-iljin <showtomid@gmail.com>
2026-06-29 23:16:59 +0700

    add model Flowers

--- flowers/models.py
+++ flowers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
-# Create your models here.
+
+class Flowers(models.Model):
+    name = models.CharField(max_length=250, blank=True)
+    slug = models.SlugField(max_length=250,  unique=True)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
---
+++ flowers/templatetags/flowers_category.py
@@ -0,0 +1,11 @@
+from django import template
+
+from flowers.models import Category, Flowers
+
+register = template.Library()
+
+
+@register.inclusion_tag('flowers/list_category.html')
+def flower_category(cat_select=0):
+    flower_cat = Category.objects.all()
+    return {'category': flower_cat, 'cat_select': cat_select}

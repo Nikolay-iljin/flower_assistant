9ab4719abb2c41a838a7d763bf38f7759f7da356
Nikolay-iljin <showtomid@gmail.com>
2026-06-30 00:29:15 +0700

    add translate to eng for slug

--- flowers/models.py
+++ flowers/models.py
@@ -3,9 +3,19 @@ from django.template.defaultfilters import slugify
 from django.urls import reverse
 
 
+def translit_to_eng(s: str) -> str:
+    d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+         'е': 'e', 'ë': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'к': 'k',
+         'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+         'с': 'c', 'т': 't', 'у': 'u', 'ф': 'f', 'x': 'h', 'ц': 'c', 'ч': 'ch',
+         'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'i', 'ъ': '', 'э': 'r', 'ю': 'yu', 'я': 'ya'}
+
+    return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
+
+
 class Flowers(models.Model):
     name = models.CharField(max_length=250, blank=True)
-    slug = models.SlugField(max_length=250, blank=True, db_index=True, default='')
+    slug = models.SlugField(max_length=250, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
@@ -14,4 +24,7 @@ class Flowers(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):  # формируем slug на основе заголовка
+        self.slug = slugify(translit_to_eng(self.name))
+        super().save(*args, **kwargs)
 

f1c82ac580c0f2087323839fd2441f27c8118ad6
Nikolay-iljin <showtomid@gmail.com>
2026-07-01 00:25:46 +0700

    create register functional

--- users/forms.py
+++ users/forms.py
@@ -10,3 +10,26 @@ class LoginUserForm(AuthenticationForm):
     class Meta():
         model = get_user_model()
         fields = ['username', 'password']
+
+
+class RegisterUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=20, label='Имя пользователя')
+    password = forms.CharField(max_length=20, label='Пароль')
+    password2 = forms.CharField(max_length=20, label='Повтори парль')
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password', 'password2', 'email', 'first_name', 'last_name']
+        labels = {'email': 'E-mail', 'first_name': 'Имя', 'last_name': 'Фамилия'}
+
+    def clean_password(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают!')
+
+        return cd['password']
+
+    def clean_email(self):
+        cd = self.cleaned_data['email']
+        if get_user_model().objects.filter(email=cd).exists():
+            raise forms.ValidationError('Пользователь с таким E-mail уже существует')
--- users/views.py
+++ users/views.py
@@ -5,24 +5,31 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from users.forms import LoginUserForm
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
 
 
-def login_user(request):
+class RegisterUserForm:
+    pass
+
+
+def register(request):
     if request.method == 'POST':
-        form = LoginUserForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('index'))
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
     else:
-        form = LoginUserForm()
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
 
-    return render(request, 'users/login.html', {'form': form})
 
 
 def logout_user(request):
     logout(request)
-    return HttpResponseRedirect(reverse('users:logout'))
+    return HttpResponseRedirect(reverse('users:login'))
